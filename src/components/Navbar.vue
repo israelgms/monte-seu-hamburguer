4e437b7f89442bc93c9fb8c7a54613da54c1eefe
israelgms <israelgomes13@hotmail.com>
2021-09-11 22:12:56 -0300

    Criação do componente Navbar e importação do mesmo no App

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="nav">
+    <router-link to="/"><img src="" id="logo"></router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+</style>
