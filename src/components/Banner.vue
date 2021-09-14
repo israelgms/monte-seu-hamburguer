13e821a95439adc0a2e4b8b1c101675759dd87c4
israelgms <israelgomes13@hotmail.com>
2021-09-14 15:31:38 -0300

    Removendo imagens desnecessárias e criando o componente Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main-banner">
+        <h1>Monte seu hamburguér</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+       name: "Banner", 
+    }
+</script>
+
+<style scoped>
+    #main-banner{
+        background-image: url('/img/burguer.jpg');
+    }
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Inicio</h1>
+    <h1>Monte o seu burguer: </h1>
   </div>
 </template>
 
 <script>
+  import Banner from "../components/Banner.vue"
+
+  export default{
+    name: "Inicio",
+    components:{
+      Banner,
+    }
+  }
+
 
 </script>
