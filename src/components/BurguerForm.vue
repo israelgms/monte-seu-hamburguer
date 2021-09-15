a45f4bc4f265123afc12933fe90f49ebdb90268c
israelgms <israelgomes13@hotmail.com>
2021-09-15 15:35:41 -0300

    Populando campos do form com dados do backend

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -3,7 +3,7 @@
         <p>Componente Mensagem</p>
     </div>
     <div>
-        <form id="burguer-form">
+        <form id="burguer-form" @submit="createBurguer">
             <h1>Monte o seu burguer: </h1>
             <div class="input-container">
                 <label for="nome">Nome do Cliente: </label>
@@ -13,39 +13,25 @@
                 <label for="pao">Escolha o pão</label>
                 <select name="pao" id="pao" v-model="pao">
                     <option value="">Selecione o seu pão</option>
-                    <option value="Integral">Integral</option>
+                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{pao.tipo}}</option>
                 </select>
             </div>    
             <div class="input-container">
                 <label for="carne">Escolha a sua carne: </label>
                 <select name="carne" id="carne" v-model="carne">
                     <option value="">Selecione o tipo de carne</option>
-                    <option value="Maminha">Maminha</option>
+                    <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{carne.tipo}}</option>
                 </select>
             </div>    
             <div id="opcionais-container" class="input-container">
                 <label id="opcionais-title" for="opcionais">Selecione os opcionais: </label>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>Salame</span>
-                </div>
-                <div class="checkbox-container">
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                    <span>Salame</span>
+                <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id" >
+                    <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                    <span>{{opcional.tipo}}</span>
                 </div>
             </div>    
-              
-                
             <div class="input-container">
-                <input type="submit" class="submit-btn" value="criar-burguer">
+                <input type="submit" class="submit-btn" value="Montar Hamburguer">
             </div>
     
         </form>
@@ -55,7 +41,39 @@
 
 <script>
 export default {
-    name: "BurguerForm"
+    name: "BurguerForm",
+    data(){
+        return{
+            paes: null,
+            carnes: null,
+            opcionaisdata: null, // plural = dados que vem do servidor. Singular = enviados
+            nome: null,
+            pao: null,
+            carne: null,
+            opcionais: [],
+            status: "Solicitado",
+            msg: null,
+        }        
+    }, 
+    methods: {
+        //Pegando os dados do "Backend"
+            async getIngredientes(){
+                const req = await fetch("http://localhost:3000/ingredientes");
+                const data = await req.json();
+
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionaisdata = data.opcionais;
+            },
+            //Enviando os dados para o "Backend"
+            async createBurguer(e){
+                e.preventDefault();
+                console.log("Criou o hamburguer")
+            }
+        },
+        mounted(){ // quando o componente for montado, chamar a função getIngredientes
+            this.getIngredientes()
+        }
 }
 </script>
 
