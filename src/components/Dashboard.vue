6dba5fd155a9198cc005e6c4d291693ba04ea89f
israelgms <israelgomes13@hotmail.com>
2021-09-15 20:39:30 -0300

    Grande avanço no componente Dashboard da view Pedidos (70%)

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,71 @@
 <template>
-    
+  <div id="burger-table">
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações:</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row">
+        <div class="order-number">1</div>
+        <div>João</div>
+        <div>Pão de trigo</div>
+        <div>Maminha</div>
+        <div>
+          <ul>
+            <li>Salame</li>
+            <li>Tomate</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">Status</option>
+          </select>
+          <button class="delete-btn">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default{
-        name: "Dashboard",
-    }
+export default {
+  name: "Dashboard",
+};
 </script>
 
 <style scoped>
 
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+#burger-table-heading div,
+.burger-table-row div{
+  width: 19%;
+}
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
 </style>
--- src/views/Pedidos.vue
+++ src/views/Pedidos.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="main-container">
-    <h1>Gerenciar Pedidos</h1>
+    <Dashboard />
   </div>
 </template>
 
 <script>
+import Dashboard from "../components/Dashboard.vue"
+
 export default {
-  name: "Pedidos"
+  name: "Pedidos",
+  components: {
+    Dashboard,
+  }
 }
 </script>
